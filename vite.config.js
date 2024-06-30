01895b17910b3e177dfb7a7461caf590e11f92b0
danfer <gdanfer07@gmail.com>
2024-06-30 11:21:00 -0500

    fin_2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  
+  plugins: [Vue()],
 })
